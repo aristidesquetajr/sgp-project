ecfa7cd99002a3b7e4e6c83c51e276c16bec2b78
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2021-07-13 20:21:50 +0100

    criaçao do readme.md

--- src/com/sgp/main/Main.java
+++ src/com/sgp/main/Main.java
@@ -13,19 +13,15 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         URI uri = Paths.get("src/com/sgp/views/Login.fxml").toAbsolutePath().toUri();
-        System.out.println("Source: " + uri);
-        
+                
         Parent root = FXMLLoader.load(uri.toURL());
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Login");
-        //stage.setResizable(false);
+        stage.setResizable(false);
         stage.show();
     }
     
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     };
